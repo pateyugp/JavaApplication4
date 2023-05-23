934b187130158905469c49fb76666f25b6def24a
pateyugp <pateyugp@sheridancollege.ca>
2023-05-23 14:33:11 -0400

    added a new part time student class class

---
+++ src/javaapplication4/Arrays.java
@@ -0,0 +1,14 @@
+package arrays;
+public class Arrays {
+    public static void main(String[] args) {
+        int[] mynumbers = new int[5];
+        mynumbers[0] = 0;
+        mynumbers[1] = 1;
+        mynumbers[2] = 2;
+        mynumbers[3] = 3;
+        mynumbers[4] = 4;
+        for(int i = 0;i < mynumbers.length;i++){
+        System.out.println(mynumbers[i]);
+        }
+    } 
+}
